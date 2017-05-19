5891d13134987e72d6331000275dfcec22e06983
Sujan <najussujan@gmail.com>
2017-05-19 11:04:43 -0400

    Decimal to binary

---
+++ src/main/java/com/najus/binaryrepresentation/BinaryRepresentation.java
@@ -0,0 +1,22 @@
+package com.najus.binaryrepresentation;
+
+public class BinaryRepresentation {
+	private StringBuilder sb = new StringBuilder();
+
+	public long convert(int input) {
+		int remainder;
+		if (input <= 1) {
+			sb.append(input);
+			return input;
+		}
+		remainder = input % 2;
+		convert(input >> 1);
+		sb.append(remainder);
+		return Long.parseLong(sb.toString());
+	}
+
+	public int bitsRequiredForRepresentation(int input) {
+		return String.valueOf(convert(input)).length();
+	}
+
+}
---
+++ src/test/java/com/najus/binaryrepresentation/BinaryRepresentationTest.java
@@ -0,0 +1,20 @@
+package com.najus.binaryrepresentation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BinaryRepresentationTest {
+
+	@Test
+	public void testBinaryConversionFor1000() {
+		BinaryRepresentation binaryRep = new BinaryRepresentation();
+		assertEquals(1111101000, binaryRep.convert(1000));
+	}
+
+	@Test
+	public void maxBitRepresentationOf1000() {
+		BinaryRepresentation binaryRep = new BinaryRepresentation();
+		assertEquals(10, binaryRep.bitsRequiredForRepresentation(1000));
+	}
+
+}
